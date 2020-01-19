57d0d4d0e616877573edb6cfdd18a8338b4ee3ee
michaltaberski <m.taberski@gmail.com>
2020-01-19 23:02:36 +0100

    scripts update

--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
 docker-compose -f ./docker-compose.yml up -d
+echo "---------------------------------"
+echo "node-red http://127.0.0.1:1880/"
+echo "grafana http://127.0.0.1:3000/"
+echo "chorograf http://localhost:8888/"
